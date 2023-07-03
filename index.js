8598bced4ec35d6dec99c0417441d56c86410adc
unknown <dinabeysinghe@gmail.com>
2023-07-03 12:52:05 +0530

    add query selector

---
+++ index.js
@@ -0,0 +1,9 @@
+let toggleButton = document.querySelector('#myButton');
+let descriptionP = document.querySelector('#myPara');
+
+toggleButton.onclick = toggleDescription;
+
+
+function toggleDescription() {
+    console.log('First');
+}
