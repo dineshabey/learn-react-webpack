7041a9cb2a22da85e62a4a9d58ec237b78190c37
unknown <dinabeysinghe@gmail.com>
2023-07-08 06:12:33 +0530

    cgange gitignore file

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";let t=document.querySelector("#myButton"),e=document.querySelector("#myPara");t.onclick=function(){n=!n,o()};let n=!1;function o(){t.textContent=n?"Hide Description":"Show Description",e.style.display=n?"block":"none "}o()})();
+(() => { "use strict"; let t = document.querySelector("#myButton"), e = document.querySelector("#myPara"); t.onclick = function () { n = !n, o() }; let n = !1; function o() { t.textContent = n ? "Hide Description" : "Show Description", e.style.display = n ? "block" : "none " } o() })();
